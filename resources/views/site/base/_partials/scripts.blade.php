9a6e8c6c3b015307614d02006f069ae1c3fc5dc9
Алексей <alexsynarchin@gmail.com>
2021-08-12 07:58:07 +0500

    Import data

--- resources/views/site/base/_partials/scripts.blade.php
+++ resources/views/site/base/_partials/scripts.blade.php
@@ -1,3 +1,3 @@
 <!-- Scripts -->
-<script src="{{ asset('assets/site/js/app.js') }}?ver=89"></script>
+<script src="{{ asset('assets/site/js/app.js') }}?ver=91"></script>
 <script src="https://kit.fontawesome.com/9b6eb0a8fc.js" crossorigin="anonymous"></script>
